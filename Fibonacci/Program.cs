bfe15b1d96594d65034fa77e3c8944637c4e3103
Yuri Chiba <yurizin163@gmail.com>
2022-06-12 23:07:49 -0300

    Fibonacci adicionado

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+    
+    int numeroAnterior = 0;
+    int numeroAtual = 1;
+    int fibonacci;
+   
+   for (int i = 0; i <5;  i++ )
+    {  
+        fibonacci = numeroAnterior + numeroAtual; 
+        numeroAnterior = numeroAtual; 
+        numeroAtual = fibonacci;   
+        Console.WriteLine  (fibonacci);
+    }
+          }
+    }
+}
